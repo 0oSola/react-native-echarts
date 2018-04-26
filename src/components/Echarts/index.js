37e6a515d8065a24b49f90936171253ddf7819b8
chengang <chengang@kingsoft.com>
2018-04-26 17:46:17 +0800

    android 兼容

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -4,21 +4,10 @@ import renderChart from './renderChart';
 import echarts from './echarts.min';
 
 export default class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.setNewOption = this.setNewOption.bind(this);
-  }
-  
-
   componentWillReceiveProps(nextProps) {
     if(nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
-  }
-
-  setNewOption(option) {
-    this.refs.chart.postMessage(JSON.stringify(option));
   }
 
   render() {
@@ -33,7 +22,7 @@ export default class App extends Component {
             backgroundColor: this.props.backgroundColor || 'transparent'
           }}
           scalesPageToFit={Platform.OS !== 'ios'}
-          source={require('./tpl.html')}
+          source={Platform.OS !== 'ios'?{uri:'file:///android_asset/tpl.html'}:require('./tpl.html')}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
       </View>
